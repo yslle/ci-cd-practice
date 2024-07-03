15546e9732453e54c1c6d78d5d0f302922769683
이승연 <dltmddus9082@naver.com>
2024-07-04 00:59:05 +0900

    Feat: @EnableJpaAuditing 추가

--- src/main/java/umc/cicd/CicdApplication.java
+++ src/main/java/umc/cicd/CicdApplication.java
@@ -2,7 +2,9 @@ package umc.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @SpringBootApplication
 public class CicdApplication {
 
